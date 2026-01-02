147343c89db906e5ab78d82281c1c210bcd74097
Onran <kotshel2009@gmail.com>
2026-01-03 02:28:06 +0900

    version changed to 0.2.0

--- src/main/java/com/github/onran0/passer/core/PasserCore.java
+++ src/main/java/com/github/onran0/passer/core/PasserCore.java
@@ -14,7 +14,7 @@ import java.util.prefs.Preferences;
 
 public class PasserCore {
 
-    public static final String VERSION = "0.1.0";
+    public static final String VERSION = "0.2.0";
 
     public static final char[] MISSING_PROPERTY_DEFAULT = "Not set".toCharArray();
     public static final int RECENT_FILES_COUNT = 10;
